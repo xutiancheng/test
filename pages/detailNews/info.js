4cc0f75eb5ab71350a34696054b8f828c56c7571
twt313 <twt313@gmail.com>
2017-01-17 16:38:08 +0800

    'add'

--- pages/detailNews/info.js
+++ pages/detailNews/info.js
@@ -1,11 +1,15 @@
 // pages/detail-info/info.js
+var WxParse = require('../wxParse/wxParse.js');
 var app = getApp();
 Page({
   data:{
     data:""
     },
   onLoad:function(options){
-    var that = this
+    var that = this;
+    this.setData({
+      'type':options.type
+    });
     wx.request({
       url: app.globalWebUrl.host+'/project/content.html',
       header: {
@@ -15,23 +19,15 @@ Page({
         "projectId":options.projectId
       },      
       success: function(res) {
-        console.log(res.data);
+        
         that.setData({
-        'data':res.data
-      })
+          'data':res.data
+        });
+
+        WxParse.wxParse('article', 'html', res.data.content, that,5);
+
       }
     })   
   },
-  onReady:function(){
-    // 页面渲染完成
-  },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
-  }
+  
 })
